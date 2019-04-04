b59cf9c848d0913ff32133bfaee5b221eb2e9edc
Infinite Machine <>
2019-04-05 00:02:34 +0300

    Added getters in Record.java.

--- DecisionTree/src/Record.java
+++ DecisionTree/src/Record.java
@@ -1,3 +1,7 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -17,5 +21,25 @@ public class Record<T> {
      * indicate that it is not.
      */
     private T target;
+
+    /**
+     * Gets an unmodifiable Map, with the Feature's' of this Record, keyed by
+     * their title.
+     * @return An unmodifiable Map, with the Feature's' of this Record, keyed by
+     * their title.
+     */
+    public @NotNull Map<String, Feature<?>> getFeatures() {
+        return Collections.unmodifiableMap(this.features);
+    }
+
+    /**
+     * Gets the class/category of this Record. Non null values indicate that the
+     * target is present, otherwise null values indicate that it is not.
+     * @return The class/category of this Record or null if the target value is
+     * not present.
+     */
+    public @Nullable T getTarget() {
+        return this.target;
+    }
 
 }//end class Record
