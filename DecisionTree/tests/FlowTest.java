f9c6f0574668e94e5a33c3267e7a819ef434dda0
Infinite Machine <>
2019-04-07 22:51:28 +0300

    Fixed a few typos and variable names in FlowTest.java.

--- DecisionTree/tests/FlowTest.java
+++ DecisionTree/tests/FlowTest.java
@@ -16,17 +16,17 @@ public class FlowTest {
         final String SALARY = "salary";
         final String SEX = "sex";
 
-        Feature<Integer> f1 = new Feature<>(AGE, 35, Feature.Type.CONTINUOUS);
-        Feature<Integer> f2 = new Feature<>(SALARY, 100_816, Feature.Type.CONTINUOUS);
-        Feature<String> f3 = new Feature<>(SEX, "male", Feature.Type.DISCRETE);
+        Feature<Integer> f1 = new Feature<>(AGE, 30, Feature.Type.CONTINUOUS);
+        Feature<Integer> f2 = new Feature<>(SALARY, 40_816, Feature.Type.CONTINUOUS);
+        Feature<String> f3 = new Feature<>(SEX, "female", Feature.Type.DISCRETE);
         Record<String> r1 = new Record<>(Arrays.asList(f1, f2, f3), null);
 
-        System.out.println(dt.predict(r1));
+        System.out.println("result: " + dt.predict(r1));
     }
 
     /**
-     * Creates test Records for the DecisionTree.
-     * @return Test Records for the DecisionTree.
+     * Creates test Record's' for the DecisionTree.
+     * @return Test Record's' for the DecisionTree.
      */
     static @NotNull Collection<Record<String>> createRecords() {
         final String AGE = "age";
@@ -34,7 +34,7 @@ public class FlowTest {
         final String SEX = "sex";
 
         final String TRG_GOOD = "good";
-        final String TRG_Bad = "bad";
+        final String TRG_BAD = "bad";
 
         Feature<Integer> f1 = new Feature<>(AGE, 25, Feature.Type.CONTINUOUS);
         Feature<Integer> f2 = new Feature<>(SALARY, 50_000, Feature.Type.CONTINUOUS);
@@ -44,7 +44,7 @@ public class FlowTest {
         f1 = new Feature<>(AGE, 51, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 45_816, Feature.Type.CONTINUOUS);
         f3 = new Feature<>(SEX, "male", Feature.Type.DISCRETE);
-        Record<String> r2 = new Record<>(Arrays.asList(f1, f2, f3), TRG_Bad);
+        Record<String> r2 = new Record<>(Arrays.asList(f1, f2, f3), TRG_BAD);
 
         f1 = new Feature<>(AGE, 42, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 75_491, Feature.Type.CONTINUOUS);
@@ -54,7 +54,7 @@ public class FlowTest {
         f1 = new Feature<>(AGE, 36, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 15_034, Feature.Type.CONTINUOUS);
         f3 = new Feature<>(SEX, "female", Feature.Type.DISCRETE);
-        Record<String> r4 = new Record<>(Arrays.asList(f1, f2, f3), TRG_Bad);
+        Record<String> r4 = new Record<>(Arrays.asList(f1, f2, f3), TRG_BAD);
 
         f1 = new Feature<>(AGE, 21, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 65_500, Feature.Type.CONTINUOUS);
@@ -64,12 +64,12 @@ public class FlowTest {
         f1 = new Feature<>(AGE, 62, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 35_000, Feature.Type.CONTINUOUS);
         f3 = new Feature<>(SEX, "male", Feature.Type.DISCRETE);
-        Record<String> r6 = new Record<>(Arrays.asList(f1, f2, f3), TRG_Bad);
+        Record<String> r6 = new Record<>(Arrays.asList(f1, f2, f3), TRG_BAD);
 
         f1 = new Feature<>(AGE, 23, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 74_154, Feature.Type.CONTINUOUS);
         f3 = new Feature<>(SEX, "male", Feature.Type.DISCRETE);
-        Record<String> r7 = new Record<>(Arrays.asList(f1, f2, f3), TRG_Bad);
+        Record<String> r7 = new Record<>(Arrays.asList(f1, f2, f3), TRG_BAD);
 
         f1 = new Feature<>(AGE, 56, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 120_000, Feature.Type.CONTINUOUS);
@@ -79,7 +79,7 @@ public class FlowTest {
         f1 = new Feature<>(AGE, 34, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 25_150, Feature.Type.CONTINUOUS);
         f3 = new Feature<>(SEX, "male", Feature.Type.DISCRETE);
-        Record<String> r9 = new Record<>(Arrays.asList(f1, f2, f3), TRG_Bad);
+        Record<String> r9 = new Record<>(Arrays.asList(f1, f2, f3), TRG_BAD);
 
         f1 = new Feature<>(AGE, 28, Feature.Type.CONTINUOUS);
         f2 = new Feature<>(SALARY, 165_000, Feature.Type.CONTINUOUS);
