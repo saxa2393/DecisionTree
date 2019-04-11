9331407537119b0a48106cd368d22b3e79eb03eb
Infinite Machine <>
2019-04-11 22:50:41 +0300

    Added Generator class, in Feature.java, to generate Feature's'.

--- DecisionTree/src/Feature.java
+++ DecisionTree/src/Feature.java
@@ -7,6 +7,61 @@ import org.jetbrains.annotations.NotNull;
 public class Feature<T extends Comparable<T>> {
 
     /**
+     * Represents a mechanism that generates Feature's'.
+     * @param <T> The type of the data the Feature's' of this Generator contain.
+     */
+    public static class Generator<T extends Comparable<T>> {
+
+        /**
+         * The title of the Feature's' this Generator creates.
+         */
+        private final String TITLE;
+
+        /**
+         * The type of the Feature's' this Generator creates.
+         */
+        private final Type TYPE;
+
+        /**
+         * Creates a Generator, given the title and the type of the Feature's'
+         * it will create.
+         * @param title The title of the Feature's' this Generator creates.
+         * @param type The type of the Feature's' this Generator creates.
+         */
+        public Generator(@NotNull String title, @NotNull Type type) {
+            this.TITLE = title;
+            this.TYPE = type;
+        }
+
+        /**
+         * Generates a Feature, given its data.
+         * @param data The data of the Feature to generate.
+         * @return A Feature, with the title and type of this Generator, and the
+         * given data.
+         */
+        public @NotNull Feature<T> generate(@NotNull T data) {
+            return new Feature<>(this.getTitle(), data, this.getType());
+        }
+
+        /**
+         * Gets the title of the Feature's' this Generator creates.
+         * @return The title of the Feature's' this Generator creates.
+         */
+        public @NotNull String getTitle() {
+            return this.TITLE;
+        }
+
+        /**
+         * Gets the type of the Feature's' this Generator creates.
+         * @return The type of the Feature's' this Generator creates.
+         */
+        public @NotNull Type getType() {
+            return this.TYPE;
+        }
+
+    }//end inner class Generator
+
+    /**
      * Represents the TYPE of the data contained by this Feature. Discrete or
      * continuous.
      */
